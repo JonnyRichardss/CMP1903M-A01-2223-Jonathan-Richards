5b96874f54f325003cf3044340c747806c88665b
Jonny <unconfigured@null.spigotmc.org>
2023-03-02 16:48:46 +0000

    added test.cs and dummy returns to pack.cs

---
+++ CMP1903M A01 2223/Testing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_A01_2223
+{
+    class Testing
+    {
+        public static void Main()
+        {
+            Pack testPack = new Pack();
+        }
+    }
+}
